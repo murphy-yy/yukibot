fa7276333a2a78eb2751a31654ac370e057bb0ac
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-05-13 19:39:08 +0900

    オーナーの自動取得、hexエラーの回避

--- colorful.py
+++ colorful.py
@@ -3,12 +3,15 @@
 import discord
 
 token = ''
-owner = 000000000000000000
 role_name = 'すごい染料'
 
 class Bot(discord.Client):
     async def on_ready(self):
         print(self.user, 'でログイン完了！')
+
+        global owner
+        owner = (await self.application_info()).owner
+        print('オーナーは', owner, 'です！')
 
     async def on_message(self, message):
         if message.author.bot:
@@ -18,15 +21,24 @@ class Bot(discord.Client):
         member = message.guild.get_member(message.author.id)
 
         if message.content == '/stop':
-            if message.author.id == owner:
+            if message.author.id == owner.id:
+                print(message.author, 'からの命令によりシャットダウン中...')
                 await channel.send('OK...')
                 await self.logout()
 
-        if message.content.startswith('/color'):
+        if message.content == '/color':
             await channel.send('何色にしますか？6文字(HEX)で答えてください。\n<https://www.google.com/search?q=color+picker>')
 
             def color_check(reply):
-                return message.author.id == reply.author.id and reply.channel == channel and reply.content.encode('utf-8').isalnum() and len(reply.content) == 6
+                if message.author.id == reply.author.id and reply.channel == channel:
+                    s = reply.content
+                    if s.encode('utf-8').isalnum() and len(s) == 6:
+                        try:
+                            int(s, 16)
+                            return True
+                        except ValueError:
+                            pass
+                return False
 
             hex = (await self.wait_for('message', check=color_check)).content
             progress = await channel.send(f'25% -> #{hex} に決定！')
@@ -43,7 +55,7 @@ class Bot(discord.Client):
             await member.add_roles(role)
             await progress.edit(content=f'100% -> 名前の色を #{hex} に変更完了！ ({member})')
 
-        if message.content.startswith('/resetcolor'):
+        if message.content == '/resetcolor':
             progress = await channel.send('名前の色をリセット中...')
             for role in member.roles:
                 if role.name == role_name:
