0f2a6d32c6b20826e6e9f36b88bbf0cbdb328f9f
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-05-13 19:44:46 +0900

    変数の名前を省略

--- colorful.py
+++ colorful.py
@@ -17,20 +17,20 @@ class Bot(discord.Client):
         if message.author.bot:
             return
 
-        channel = message.channel
+        ch = message.channel
         member = message.guild.get_member(message.author.id)
 
         if message.content == '/stop':
             if message.author.id == owner.id:
                 print(message.author, 'からの命令によりシャットダウン中...')
-                await channel.send('OK...')
+                await ch.send('OK...')
                 await self.logout()
 
         if message.content == '/color':
-            await channel.send('何色にしますか？6文字(HEX)で答えてください。\n<https://www.google.com/search?q=color+picker>')
+            await ch.send('何色にしますか？6文字(HEX)で答えてください。\n<https://www.google.com/search?q=color+picker>')
 
             def color_check(reply):
-                if message.author.id == reply.author.id and reply.channel == channel:
+                if message.author.id == reply.author.id and reply.channel == ch:
                     s = reply.content
                     if s.encode('utf-8').isalnum() and len(s) == 6:
                         try:
@@ -41,27 +41,27 @@ class Bot(discord.Client):
                 return False
 
             hex = (await self.wait_for('message', check=color_check)).content
-            progress = await channel.send(f'25% -> #{hex} に決定！')
+            p = await ch.send(f'25% -> #{hex} に決定！')
 
-            await progress.edit(content='50% -> 古い権限の修正中...')
+            await p.edit(content='50% -> 古い権限の修正中...')
             for role in member.roles:
                 if role.name == role_name:
                     print(member, 'から', role, 'を削除中...')
                     await role.delete()
 
-            await progress.edit(content='75% -> 新しい権限の準備中...')
+            await p.edit(content='75% -> 新しい権限の準備中...')
             role = await message.guild.create_role(name=role_name, color=discord.Colour(int(hex, 16)))
 
             await member.add_roles(role)
-            await progress.edit(content=f'100% -> 名前の色を #{hex} に変更完了！ ({member})')
+            await p.edit(content=f'100% -> 名前の色を #{hex} に変更完了！ ({member})')
 
         if message.content == '/resetcolor':
-            progress = await channel.send('名前の色をリセット中...')
+            p = await ch.send('名前の色をリセット中...')
             for role in member.roles:
                 if role.name == role_name:
                     print(member, 'から', role, 'を削除中...')
                     await role.delete()
-            await progress.edit(content='名前の色をリセットしました！')
+            await p.edit(content='名前の色をリセットしました！')
 
 print('ログイン中...')
 bot = Bot()
