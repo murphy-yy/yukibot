c8e96baaa76452e55751ce2f27c5bee061165e68
YukiLeafX <yukileafx@gmail.com>
2020-07-19 14:49:47 +0900

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 if __name__ == "__main__":
     token = os.environ["TOKEN"]
     print("準備中...")
-    bot = commands.Bot(command_prefix="/", description="YukiBot v2.0")
+    bot = commands.Bot(command_prefix="/", description="YukiBot v2.0 β1 [2020/07/19]")
     bot.add_cog(cogs.managecog.ManageCog(bot))
     print("起動中...")
     bot.run(token)
