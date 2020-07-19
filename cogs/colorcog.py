a50ec55e86b28b558ee517e6fd5402bf4b0f2d22
YukiLeafX <yukileafx@gmail.com>
2020-07-19 23:49:46 +0900

    色のリセットとクリーンアップコマンドを追加

--- cogs/colorcog.py
+++ cogs/colorcog.py
@@ -21,8 +21,11 @@ class ColorCog(commands.Cog):
         r, g, b = rgb
         return (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
 
-    def color_roles(self, member):
+    def color_roles_of(self, member):
         return [r for r in member.roles if r.name == self.role_name]
+
+    def empty_color_roles(self, guild):
+        return [r for r in guild.roles if r.name == self.role_name and not r.members]
 
     @commands.command(help="名前に色を付けます。")
     async def color(self, ctx):
@@ -42,7 +45,7 @@ class ColorCog(commands.Cog):
         member = discord.utils.find(lambda m: m.id == ctx.author.id, ctx.guild.members)
 
         p = await ctx.send("`(1/3)` 古い権限を削除しています...")
-        for r in self.color_roles(member):
+        for r in self.color_roles_of(member):
             await r.delete()
 
         await p.edit(content="`(2/3)` 新しい権限を作成しています...")
@@ -52,3 +55,20 @@ class ColorCog(commands.Cog):
         await member.add_roles(r)
 
         await p.edit(content=f"名前の色を {hx} ({i}) に変更しました。")
+
+    @commands.command(help="名前の色をリセットします。")
+    async def resetcolor(self, ctx):
+        member = discord.utils.find(lambda m: m.id == ctx.author.id, ctx.guild.members)
+
+        p = await ctx.send("色の権限を削除しています...")
+        for r in self.color_roles_of(member):
+            await r.delete()
+        await p.edit(content="名前の色をリセットしました！")
+
+    @commands.command(help="不要な色の権限を削除します。")
+    async def cleanupcolor(self, ctx):
+        p = await ctx.send("不要な色の権限を削除しています...")
+        targets = self.empty_color_roles(ctx.guild)
+        for r in targets:
+            await r.delete()
+        await p.edit(content=f"不要な {len(targets)} 個の {self.role_name} を削除しました。")
