3ce87dd0e7b06ccb6fa13477cdebb51fa43d996b
YukiLeafX <yukileafx@gmail.com>
2020-07-20 00:01:39 +0900

    更新

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     token = os.environ["TOKEN"]
 
     print("準備中...")
-    bot = commands.Bot(command_prefix="/", description="YukiBot v2.0 β2 [2020/07/19 更新]")
+    bot = commands.Bot(
+        command_prefix="/", description="YukiBot v2.0 β2 [2020/07/20 更新]"
+    )
     bot.add_cog(ManageCog(bot))
     bot.add_cog(ColorCog(bot))
 
