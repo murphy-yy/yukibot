9f5172d9cee97c0f484db993202c493fd0575f1f
YukiLeafX <yukileafx@gmail.com>
2020-10-17 14:22:36 +0900

    読み上げ時のエラーをもっと分かりやすくする

--- src/main/kotlin/io/github/yukileafx/yukibot/talk/TalkManager.kt
+++ src/main/kotlin/io/github/yukileafx/yukibot/talk/TalkManager.kt
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 import java.io.File
 import java.net.URL
 import java.net.URLEncoder
+import java.nio.file.Files
 import java.util.*
 
 class TalkManager : ListenerAdapter() {
@@ -16,7 +17,7 @@ class TalkManager : ListenerAdapter() {
     private val worker = DefaultAudioPlayerManager().also { AudioSourceManagers.registerLocalSource(it) }
     private val binds = mutableMapOf<String, Set<String>>().withDefault { setOf() }
     private val schedulers = mutableMapOf<String, TrackScheduler>()
-    private val dataDir = File(System.getProperty("java.io.tmpdir"), "yomi")
+    private val dataDir = Files.createTempDirectory("yomi").toFile()
 
     fun bind(vc: VoiceChannel, read: TextChannel) {
         binds[vc.id] = binds.getValue(vc.id).toMutableSet().apply { add(read.id) }
@@ -59,11 +60,13 @@ class TalkManager : ListenerAdapter() {
 
     private fun File.toOpusWithFFmpeg(): File {
         val opus = dataDir.also { it.mkdirs() }.resolve("${UUID.randomUUID()}.opus")
-        ProcessBuilder(listOf("ffmpeg", "-y", "-i", "$this", "-acodec", "libopus", "$opus"))
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
+        val log = Files.createTempFile("ffmpeg", ".log").toFile()
+        val exitCode = ProcessBuilder(listOf("ffmpeg", "-y", "-i", "$this", "-acodec", "libopus", "$opus"))
+            .redirectOutput(log)
+            .redirectError(log)
             .start()
             .waitFor()
+        check(exitCode == 0) { log.readText() }
         delete()
         return opus
     }
@@ -74,8 +77,15 @@ class TalkManager : ListenerAdapter() {
         }
         binds.filterValues { it.contains(event.channel.id) }.map { it.key }.forEach { vcId ->
             val vc = event.jda.getVoiceChannelById(vcId) ?: return
-            val src = event.message.contentDisplay.toYukkuriVoiceURL().downloadMp3().toOpusWithFFmpeg()
-            queue(vc, src.path)
+            val text = event.message.contentDisplay
+            println("${event.guild}, ${event.channel}, ${event.author}: $text")
+            val url = text.toYukkuriVoiceURL()
+            println("\t$url")
+            val mp3 = url.downloadMp3()
+            println("\t-> $mp3")
+            val opus = mp3.toOpusWithFFmpeg()
+            println("\t-> $opus")
+            queue(vc, opus.path)
         }
     }
 }
