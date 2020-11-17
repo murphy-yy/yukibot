5ef7a6269467e235803c123ad89ac9a0efc43a31
YukiLeafX <yukileafx@gmail.com>
2020-11-17 08:04:34 +0000

    update

--- cog_youtube.py
+++ cog_youtube.py
@@ -9,19 +9,19 @@ from discord.ext import commands
 class YouTube(commands.Cog):
 
     @commands.command(usage='<動画ソース> <秒数>', help='5秒間の動画切り抜きを作成します。 例: /p5 JGwWNGJdvx8 3:30')
-    async def p5(self, ctx, src, start_time):
+    async def p5(self, ctx, video_src, start_time):
         full = pathlib.Path(tempfile.NamedTemporaryFile(suffix='.mp4').name)
         edited = pathlib.Path(tempfile.NamedTemporaryFile(suffix='.mp4').name)
 
-        progress = await ctx.send('ダウンロード中...')
+        x = await ctx.send('ダウンロード中...')
         subprocess.check_call(
-            f'youtube-dl -f best -o {full} -- {src}', shell=True)
+            f'youtube-dl -f best -o {full} -- {video_src}', shell=True)
 
-        await progress.edit(content='変換中...')
+        await x.edit(content='変換中...')
         subprocess.check_call(
             f'ffmpeg -ss {start_time} -i {full} -t 0:05 {edited}', shell=True)
 
-        await progress.delete()
+        await x.delete()
 
         upload = discord.File(edited)
         await ctx.send(file=upload)
