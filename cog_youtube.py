1830969b4a8444ee5f3f96f1d0330733a5aa0b13
YukiLeafX <yukileafx@gmail.com>
2020-11-17 15:28:30 +0000

    playbackコマンドに大幅アップデート

--- cog_youtube.py
+++ cog_youtube.py
@@ -8,20 +8,45 @@ from discord.ext import commands
 
 class YouTube(commands.Cog):
 
-    @commands.command(usage='<動画ソース> <秒数>', help='5秒間の動画切り抜きを作成します。 例: /p5 JGwWNGJdvx8 3:30')
-    async def p5(self, ctx, video_src, start_time):
-        full = pathlib.Path(tempfile.NamedTemporaryFile(suffix='.mp4').name)
-        edited = pathlib.Path(tempfile.NamedTemporaryFile(suffix='.mp4').name)
+    @commands.command(usage='<動画ソース> [再生を開始する秒数] [再生する秒数]', help='動画を再生します。 例: /playback "youtu.be/TQ8WlA2GXbk" 1:33 0:12', aliases=['play', 'p', 'p5'])
+    async def playback(self, ctx, video_src, start_time: str = '0:00', duration_time: str = '0:30'):
+        sel_entry = ['bestvideo[height<=360][ext=webm]+bestaudio[ext=webm]',
+                     'bestvideo[height<=360]+bestaudio',
+                     'best[ext=webm][height<=360]',
+                     'best[height<=360]',
+                     'best']
 
-        x = await ctx.send('ダウンロード中...')
-        subprocess.check_call(
-            f'youtube-dl -f best -o {full} -- {video_src}', shell=True)
+        cmd1 = ['youtube-dl',
+                '-f', '/'.join(sel_entry),
+                '--get-filename',
+                video_src]
 
-        await x.edit(content='変換中...')
-        subprocess.check_call(
-            f'ffmpeg -ss {start_time} -i {full} -t 0:05 {edited}', shell=True)
+        x = await ctx.send(f'ファイル名を検証しています...\n{cmd1}')
+        filename = subprocess.check_output(cmd1).decode().rstrip()
+
+        p = pathlib.Path(filename)
+        tmp1 = pathlib.Path(tempfile.NamedTemporaryFile(suffix=p.suffix).name)
+        tmp2 = tmp1.parent / f'{tmp1.stem}-cropped{p.suffix}'
+
+        cmd2 = ['youtube-dl',
+                '-f', '/'.join(sel_entry),
+                '-o', tmp1,
+                video_src]
+
+        cmd3 = ['ffmpeg',
+                '-ss', start_time,
+                '-i', tmp1,
+                '-t', duration_time,
+                '-c', 'copy',
+                tmp2]
+
+        await x.edit(content=f'ダウンロードを実行しています...\n{cmd2}')
+        subprocess.check_call(cmd2)
+
+        await x.edit(content=f'変換しています...\n{cmd3}')
+        subprocess.check_call(cmd3)
 
         await x.delete()
 
-        upload = discord.File(edited)
+        upload = discord.File(tmp2)
         await ctx.send(file=upload)
